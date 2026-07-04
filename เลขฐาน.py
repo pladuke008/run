4187dab62e98d433db2a0b22c25323e40b03e502
pladuke008 <xafhamxinthcho@gmail.com>
2026-07-04 15:36:30 +0700

    Add files via upload

---
+++ เลขฐาน.py
@@ -0,0 +1,15 @@
+dec = 2569 #เลขฐาน10
+conv_to = bin(dec) #แปลงเลขฐาน10 to 2
+dec = (conv_to)
+print(dec)
+
+dec2 = 2569 #เลขฐาน10
+conv_to2 = oct(dec2) #แปลงเลขฐาน10 to 8
+dec2 = (conv_to2)
+print(dec2)
+
+dec3 = 2569 #เลขฐาน10
+conv_to3 = hex(dec3) #แปลงเลขฐาน10 to 16
+dec3 = (conv_to3)
+print(dec3)
+print("อัฟฮัม อินทโช")
